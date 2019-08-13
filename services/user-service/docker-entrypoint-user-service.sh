f4a74d68729e62b8dd5998ac86b2d54948b12af6
Chris Ro <chris.ro@kareo.com>
2019-08-12 23:49:35 -0700

    dockerized user-dashboard and api-gateway-zuul-server, and created docker entrypoints for user-dashboard and user-service checking for the UP status of the config and eureka server using nc

---
+++ services/user-service/docker-entrypoint-user-service.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+while ! nc -z config-server-container 20001 ; do
+    echo "Waiting for the Config Server"
+    sleep 3
+done
+
+while ! nc -z eureka-server-container 20002 ; do
+    echo "Waiting for the Eureka Server"
+    sleep 3
+done
+
+java -jar /user-service.jar
